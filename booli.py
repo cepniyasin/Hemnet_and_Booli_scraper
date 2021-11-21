481ca4ad772e86dfe58442437243cc1de29faead
Yasin Cepnioglu <yasincepnioglu@gmail.com>
2021-11-21 23:09:45 +0100

    booli functioning

--- booli.py
+++ booli.py
@@ -1,164 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import datetime
 import time
-
-
-# def hemnet_parser():
-#     try:
-#         main = [WebDriverWait(driver, 10).until(
-#             EC.presence_of_all_elements_located((By.XPATH,
-#                                         "//li[@class='normal-results__hit js-normal-list-item']")))]
-#         for articles in main:
-#
-#             for article in articles:
-#                 global counter
-#                 counter += 1
-#                 link = article.find_element(By.CLASS_NAME, "listing-card").get_attribute('href')
-#                 apartment = article.find_element(By.CLASS_NAME, "listing-card__address").text.split("\n")
-#                 address = apartment[0]
-#                 location = apartment[1]
-#                 attributes = article.find_element(By.CLASS_NAME, "listing-card__attributes-row")
-#                 attribute_list = attributes.text.split("\n")
-#                 price = attribute_list[0]
-#                 area = None
-#                 room = None
-#                 if len(attribute_list) == 2:
-#                     area = attribute_list[1]
-#                 elif len(attribute_list) == 3:
-#                     area = attribute_list[1]
-#                     room = attribute_list[2]
-#
-#                 property_type_list = article.find_element(By.CLASS_NAME, 'listing-card__location'
-#                                                      ).get_attribute("textContent").split("\n")
-#                 property_type = property_type_list[2]
-#                 write_csv(str(counter), link, address, location, price, area, room, property_type)
-#
-#     except Exception as e:
-#         print(e)
-#         driver.close()
-
-
-def hemnet_sold_parser():
-    main = [WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH,
-                                                 "//div[@class='_2m6km uC2y2 _3oDFL']")))]
-    # print(main[0][0].text)
-
-    all_articles = main[0][0].text
-
-    all_articles = all_articles.split("%\n")
-
-    # print(all_articles)
-
-    all_articles_cleaned = []
-
-    for i in all_articles:
-        x = i.split("\n")
-        if len(x) > 13:
-            y = ""
-            for n in x:
-                y += n + '\n'
-            y = y.split('—')
-            for v in y:
-                v = v.split("\n")
-                z = []
-                for q in v:
-                    if len(q) > 0:
-                        z.append(q)
-                print(z)
-                print(len(z))
-                all_articles_cleaned.append(z)
-
-        elif len(x) > 2:
-            all_articles_cleaned.append(x)
-            print(x)
-            print(len(x))
-    # print(all_articles_cleaned)
-
-    # # try:
-    #     main = [WebDriverWait(driver, 10).until(
-    #         EC.presence_of_all_elements_located((By.XPATH,
-    #                                                      "//li[@class='sold-results__normal-hit']")))]
-    #
-    #     print(main)
-    #     for articles in main:
-    #         for article in articles:
-    #             # print(article.text)
-    #             global counter
-    #             counter += 1
-    #             link = article.find_element(By.XPATH,
-    #                                              "//a[@class='sold-property-link']").get_attribute('href')
-    #             apartment = article.find_element(By.CLASS_NAME, "sold-property-listing__location").text.split("\n")
-    #             address = apartment[0]
-    #             print (address)
-    #             location = apartment[2]
-    #             property_type = apartment[1]
-    #             price_list = article.find_elements(By.CLASS_NAME, "sold-property-listing__price-info")
-    #             x = price_list[0].text.split("\n")
-    #             avgift = None
-    #             land_area = None
-    #             price = x[0]
-    #             date_of_sale = x[1]
-    #             # print(price, date_of_sale)
-    #
-    #
-    #
-    #             sizes = article.find_elements(By.CLASS_NAME, "sold-property-listing__size")
-    #             # print (sizes[0])
-    #                 # article.find_element(By.CLASS_NAME, "clear-children").text.split("\n")
-    #             a = sizes[0].text.split("\n")
-    #             # print(a)
-    #             area = a[0]
-    #             # print(area)
-    #             if len(a) > 1:
-    #                 if "m²" in a[1]:
-    #                     land_area = a[1]
-    #                 if "kr/mån" in a[1]:
-    #                     avgift = a[1]
-    #
-    #                 # print(avgift)
-    #                 # print(land_area)
-    #             write_csv_sold(str(counter), link, address, location, price, area,land_area, avgift, property_type, date_of_sale)
-    # #
-    # # except Exception as e:
-    # #     print(e)
-    # #     driver.close()
-
-#
-# def start_csv():
-#     date = str(datetime.datetime.today()).split()[0]
-#     global csv_file
-#     csv_file = open(f'{date +"-"+ location}.csv', 'w')
-#     csv_file.write(f'{date}' + "\n")
-#     csv_file.write('Count'
-#                    + ';' + 'Subject'
-#                    + ';' + 'Location'
-#                    + ';' + 'Price'
-#                    + ';' + 'Area'
-#                    + ';' + 'Number of rooms'
-#                    + ';' + 'Property Type'
-#                    + ';' + 'Status'
-#                    + ';' + 'Website Link'
-#                    + "\n")
-#
-#
-# def write_csv(id, link, address, location, price, area, room, property_type, date_of_sale="For Sale"):
-#     global csv_file
-#     csv_file.write(f'{id}'
-#                    + ';' + f'{address}'
-#                    + ';' + f'{location}'
-#                    + ';' + f'{price}'
-#                    + ';' + f'{area}'
-#                    + ';' + f'{room}'
-#                    + ';' + f'{property_type}'
-#                    + ';' + f'{date_of_sale}'
-#                    + ';' + f'{link}'
-#                    + "\n")
 
 
 def start_csv_sold():
@@ -168,51 +14,85 @@ def start_csv_sold():
     csv_file.write(f'{date}' + "\n")
     csv_file.write('Count'
                    + ';' + 'Subject'
-                   + ';' + 'Location'
-                   + ';' + 'Price'
                    + ';' + 'Area and number of rooms'
-                   + ';' + 'Land Size'
-                   + ';' + 'Avgift'
+                   + ';' + 'Price'
+                   + ';' + 'Location'
                    + ';' + 'Property Type'
-                   + ';' + 'Status'
-                   + ';' + 'Website Link'
+                   + ';' + 'Sold Date'
                    + "\n")
 
 
-def write_csv_sold(id, link, address, location, price, area,land_area, avgift, property_type, date_of_sale="For Sale"):
+def write_csv_sold(id, address, location, area, property_type, price, date_of_sale="For Sale"):
     global csv_file
     csv_file.write(f'{id}'
                    + ';' + f'{address}'
                    + ';' + f'{location}'
                    + ';' + f'{price}'
                    + ';' + f'{area}'
-                   + ';' + f'{land_area}'
-                   + ';' + f'{avgift}'
                    + ';' + f'{property_type}'
                    + ';' + f'{date_of_sale}'
-                   + ';' + f'{link}'
                    + "\n")
 
-# def ask_slutpris():
-#     slutpris = input("Do you want to make the search for sold properties? [y/n]: ")
-#     if slutpris == "y":
-#         return True
-#     if slutpris == "n":
-#         return False
-#     else:
-#         ask_slutpris()
+
+def hemnet_sold_parser():
+    time.sleep(7)
+
+    article_list = []
+
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH,
+                                             "//div[@class='_1EcEF iF9uQ']")))
+    for i in main:
+        address = None
+        area = None
+        location = None
+        property_type = None
+        if i:
+            article = i.text.split('\n')
+            address = article[0]
+            global counter
+
+            if len(article) == 3:
+                area = article[1]
+                location = article[2].split(", ")[1]
+                property_type = article[2].split(", ")[0]
+
+            if len(article) == 2:
+                location = article[1].split(", ")[1]
+                property_type = article[1].split(", ")[0]
+
+            article_list.append([address, area, location, property_type])
+
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH,
+                                            "//div[@class='_1EcEF MJ2C2']")))
+    index_count = 0
+    for i in main:
+        price = None
+        date_of_sale = None
+        if i:
+            article2 = i.text.split('\n')
+            price = article2[0]
+            date_of_sale = article2[-1]
+            article_list[index_count].append(price)
+            article_list[index_count].append(date_of_sale)
+        index_count += 1
+
+    for item in article_list:
+        counter += 1
+        write_csv_sold(str(counter), item[0], item[1], item[2], item[3], item[4], item[5])
 
 
-location = input("Give a location to search: ")
-
-# sold = ask_slutpris()
+location = input("Give a name to output CSV file: ")
+search_page = input("\nThe link to the starting page of the search: ")
 
 start = time.time()
 
 s = Service("chromedriver.exe")
 driver = webdriver.Chrome(service=s)
 
-driver.get('https://www.booli.se/slutpriser/sverige/77104?objectType=Fritidshus')
+driver.get(f'{search_page}')
+time.sleep(2)
 driver.implicitly_wait(5)
 
 element = WebDriverWait(driver, 10).until(
@@ -221,30 +101,39 @@ element.click()
 driver.implicitly_wait(5)
 
 counter = 0
+count_error = 0
 
 start_csv_sold()
+
 hemnet_sold_parser()
 
 next_page = True
-
+page = 0
 while next_page:
     try:
-            element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, "//button[contains(text(),'Nästa sida')]")))
-            element.click()
-            time.sleep(1)
-            driver.implicitly_wait(5)
-            hemnet_sold_parser()
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//button[contains(text(),'Nästa sida')]")))
+        element.click()
+        page += 1
+        print("Counter:", counter)
+        print('PAGE: ',page)
+        driver.implicitly_wait(5)
+        hemnet_sold_parser()
 
     except Exception as e:
         print(e)
+        csv_file.write("\nUntil page: " + f'{page}' + "\n" + "Error: "+f'{count_error}')
         csv_file.close()
-        print("\nALL FINISHED - You can find the results csv file in the same directory.")
+        print("\nALL FINISHED - You can find the results' csv file in the same directory.")
         next_page = False
         driver.quit()
 
 end = time.time()
+
 print('\nElapsed time:', int(end - start), 'seconds')
 print("Article found:", counter)
+print("Number of error:", count_error)
+
 input("\nPress return to quit.\n\n\ncepniyasin.com")
+print('Finished on page: ', page)
